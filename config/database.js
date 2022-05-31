853c9c19df3f5b1d35d2ec45eb258e3df41366e8
Onebestever <carla.h1248@gmail.com>
2022-05-30 23:09:39 -0400

    first push

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://carlaH:yX12HROSbmE3jLWV@cluster0.y1avr.mongodb.net/?retryWrites=true&w=majority',
+    'dbName': 'savAuth'
+};
